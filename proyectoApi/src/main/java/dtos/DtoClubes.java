0d6f99bc9672bfc487b632d92653edecbab11f29
ivannn05 <145344382+ivannn05@users.noreply.github.com>
2024-10-29 13:35:46 +0100

    Codigo con Login y Dar alta club
    Os dejo el codigo de Login y Dar alta con los DTOS

---
+++ proyectoApi/src/main/java/dtos/DtoClubes.java
@@ -0,0 +1,66 @@
+package dtos;
+
+import java.time.LocalDate;
+
+public class DtoClubes {
+
+	Long id;
+	String nombre = "aaaa";
+	String clave = "aaaa";
+	String contrasenia = "aaaa";
+	LocalDate fechaCreacion = LocalDate.now();
+
+	public DtoClubes() {
+		super();
+	}
+
+	public DtoClubes(Long id, String nombre, String clave, String contrasenia, LocalDate fechaCreacion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.clave = clave;
+		this.contrasenia = contrasenia;
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	public LocalDate getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(LocalDate fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+}
